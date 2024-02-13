5d3959826945e66606d64f28b76456082f18dd47
Luis Agreda <luis.mario.agreda@outlook.com>
2024-02-12 21:02:50 -0500

    password checker commit

---
+++ Beginner Projects/common_password_checker/password_checker.py
@@ -0,0 +1,23 @@
+def check_password(user_password: str):
+    with open('10-million-password-list-top-100000.txt', 'r') as file:
+        common_passwords: list[str] = file.read().splitlines()
+
+    for i, common_passwords in enumerate(common_passwords, start=1):
+        if user_password == common_passwords:
+            print(f'{user_password}    ⚠️⚠️ Common Password (#{i}) ⚠️⚠️')
+            return
+        if user_password == '':
+            print('Type something...')
+            return
+    print('✅✅ Unique Password! ✅✅')
+
+
+if __name__ == '__main__':
+    while True:
+        password: str = input("Enter a password (To exit type 'exit'): ")
+
+        if password == 'exit':
+            print('Thank you!')
+            break
+
+        check_password(password)
